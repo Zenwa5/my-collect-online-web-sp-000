c4ca682cd2922a38e33f0c8f17d0f8ac33e7b7be
Ezenwa Onukwue <ezenwa.onukwue@salesloft.com>
2018-11-16 23:04:51 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -4,7 +4,7 @@ def my_collect(empty_array)
   collection = []
   i = 0
   while empty_array.length > i
-    collection << yield empty_array[i]
+    collection << yield(empty_array[i])
       i += 1
   end
    collection
